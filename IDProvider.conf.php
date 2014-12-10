7b5b4fc31b837454185a4d89bbcc07dd97eecfa9
root <david.rousse@dsi.cnrs.fr>
2014-12-10 14:58:11 +0100

    Mantis 35404

--- IDProvider.conf.php
+++ IDProvider.conf.php
@@ -1,6 +1,7 @@
 <?php
 $IDProviders["urn:mace:cnrs.fr:janus-rec.dsi.cnrs.fr"]["SSO"] = "https://janus-rec.dsi.cnrs.fr/shibboleth-idp-recette/SSO";
-$IDProviders["urn:mace:cnrs.fr:janus-rec.dsi.cnrs.fr"]["Name"] = "Personnel des unit&eacute;s CNRS";
+// mantis 35404 $IDProviders["urn:mace:cnrs.fr:janus-rec.dsi.cnrs.fr"]["Name"] = "Personnel des unit&eacute;s CNRS";
+$IDProviders["urn:mace:cnrs.fr:janus-rec.dsi.cnrs.fr"]["Name"] = "Personnel rattach&eacute; &agrave; des unit&eacute;s CNRS";
 $IDProviders["https://janus-rec-ext.dsi.cnrs.fr/"]["SSO"] = "https://janus-rec-ext.dsi.cnrs.fr/shibboleth-idp-recette-ext/profile/Shibboleth/SSO";
 $IDProviders["https://janus-rec-ext.dsi.cnrs.fr/"]["Name"] = "Externes";
 ?>
